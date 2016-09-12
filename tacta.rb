a913a958198f5907a309f9ed28386f2430410401
Ecthelion3 <matthijs.vandenberg@gmail.com>
2016-09-12 10:26:25 +0200

    tacta_sinatra

---
+++ tacta.rb
@@ -0,0 +1,78 @@
+require 'sinatra'
+require 'sinatra/reloader' if development?
+require './contacts_file'
+
+set :port, 4567
+
+get '/' do
+  "<h1>Tacta Contact Manager</h1>"
+end
+
+get '/contacts' do
+  @contacts = read_contacts
+  erb :'contacts/index'
+end
+
+get '/contacts/new' do
+  erb :'contacts/new'
+end
+
+post '/contacts/search' do
+  @contacts = read_contacts
+  @search_results = @contacts.select do |contact|
+    if contact[:name] =~ /#{params[:query]}/i
+      true
+    end
+  end
+  erb :'contacts/search'
+end
+
+get '/contacts/:i' do
+  @i = params[:i].to_i
+  contacts = read_contacts
+  @contact = contacts[@i]
+  erb :'contacts/show'
+end
+
+get '/contacts/:i/edit' do
+  @i = params[:i].to_i
+
+  contacts = read_contacts
+  @contact = contacts[@i]
+
+  erb :'contacts/edit'
+end
+
+get '/contacts/:i/delete' do
+  i = params[:i].to_i
+
+  contacts = read_contacts
+  contacts.delete_at(i)
+  write_contacts(contacts)
+
+  redirect "/contacts"
+end
+
+post '/contacts' do
+  new_contact = { name: params[:name], phone: params[:phone], email: params[:email] }
+
+  contacts = read_contacts
+  contacts << new_contact
+  write_contacts(contacts)
+
+  i = contacts.length - 1
+
+  redirect "/contacts/#{i}"
+end
+
+post '/contacts/:i/update' do
+  i = params[:i].to_i
+
+  updated_contact = { name: params[:name], phone: params[:phone], email: params[:email] }
+
+  contacts = read_contacts
+  contacts[i] = updated_contact
+  write_contacts(contacts)
+
+  redirect "/contacts/#{i}"
+end
